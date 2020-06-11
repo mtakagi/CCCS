2a8b05080db628c8828dead643d48b85ecb6ff8b
mtakagi <mtakagi127@live.jp>
2020-06-12 00:10:41 +0900

    Add unit test

---
+++ Assets/Editor/Compiler.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace CCCS
+{
+    public class Compiler
+    {
+        public static string Compile(string str)
+        {
+            var tokenizer = new CCCS.Tokenizer(str);
+            var token = tokenizer.Tokenize();
+            var lexer = new CCCS.TokenLexer(token);
+            var node = lexer.Expr();
+            var sb = new StringBuilder();
+
+            sb.Append(".intel_syntax noprefix\n");
+            sb.Append(".globl _main\n");
+            sb.Append("_main:\n");
+
+            sb.Append(lexer.CodeGen(node));
+            sb.Append("  pop rax\n");
+            sb.Append("  ret\n");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Assets/Editor/Test.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using CCCS;
+
+public class CCCSTest
+{
+    private static string OutputFileName = "test.s";
+    private static string OutputExeFileName = "test";
+
+    [Test]
+    public void テスト42()
+    {
+        AssertEqual(0, "0");
+        AssertEqual(42, "42");
+    }
+
+    [Test]
+    public void テスト加減算()
+    {
+        AssertEqual(21, "5+20-4");
+        AssertEqual(41, "12 + 34 - 5");
+    }
+
+    [Test]
+    public void テスト四則演算()
+    {
+        AssertEqual(47, "5+6*7");
+        AssertEqual(15, "5*(9-6)");
+        AssertEqual(4, "(3+5)/2");
+    }
+
+    [Test]
+    public void テスト単項演算子()
+    {
+        AssertEqual(10, "-10+20");
+        AssertEqual(10, "- -10");
+        AssertEqual(10, "- - +10");
+    }
+
+    [Test]
+    public void テスト比較演算子1()
+    {
+        AssertEqual(0, "0==1");
+        AssertEqual(1, "42==42");
+        AssertEqual(1, "0!=1");
+        AssertEqual(0, "42!=42");
+    }
+
+    [Test]
+    public void テスト比較演算子2()
+    {
+        AssertEqual(1, "0<1");
+        AssertEqual(0, "1<1");
+        AssertEqual(0, "2<1");
+        AssertEqual(1, "0<=1");
+        AssertEqual(1, "1<=1");
+        AssertEqual(0, "2<=1");
+    }
+
+    [Test]
+    public void テスト比較演算子3()
+    {
+        AssertEqual(1, "1>0");
+        AssertEqual(0, "1>1");
+        AssertEqual(0, "1>2");
+        AssertEqual(1, "1>=0");
+        AssertEqual(1, "1>=1");
+        AssertEqual(0, "1>=2");
+    }
+
+    private void AssertEqual(int expect, string code)
+    {
+        var path = Write(Compiler.Compile(code));
+        Compile(path);
+        Assert.AreEqual(expect, Execute(path));
+    }
+
+    private string Write(string asm)
+    {
+        var path = Path.GetTempPath();
+        File.WriteAllText(Path.Combine(path, OutputFileName), asm, Encoding.UTF8);
+
+        return path;
+    }
+
+    private void Compile(string path)
+    {
+        var info = new ProcessStartInfo("cc", $"-o {OutputExeFileName} {OutputFileName}");
+
+        info.WorkingDirectory = path;
+
+        using (var proc = Process.Start(info))
+        {
+            proc.WaitForExit();
+        }
+    }
+
+    private int Execute(string path)
+    {
+        using (var proc = Process.Start(Path.Combine(path, OutputExeFileName)))
+        {
+            proc.WaitForExit();
+            return proc.ExitCode;
+        }
+    }
+}
